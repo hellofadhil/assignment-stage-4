0a6753d214c63e6fa9a309686b65b611f3b4dee7
musaera <saefulromadoni59@smk.belajar.id>
2025-05-05 21:43:08 +0700

    add logo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   viewport:
     "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
   generator: "v0.dev",
+  icons: {
+    icon: "/logo.webp",
+  },
 };
 
 export default function RootLayout({
